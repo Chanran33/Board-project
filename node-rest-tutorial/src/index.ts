bafd1488cf2295d2410898a639b6c0843fb44edd
Chanran33 <k55416325@gmail.com>
2021-12-02 23:52:36 +0900

    json 데이터 보내고 받기

--- node-rest-tutorial/src/index.ts
+++ node-rest-tutorial/src/index.ts
@@ -2,6 +2,11 @@ import express from 'express';
 
 const app = express();
 
+//express 버전 4에는 body-parser가 내장되어있다고 했지만
+//json을 파싱하겠다는 설정을 하지 않아서 빈 객체가 온다.
+//json을 파싱하기 위해서 아래 설정을 추가 해야 한다.
+app.use(express.json());
+
 //express에는 json 데이터를 파싱하는 모듈이 내장되어있다.
 //하지만 json만 되고 x-www-form-urlencoded를 파싱하기 위해서 아래를 확장해야 한다.
 app.use(express.urlencoded({
@@ -42,6 +47,24 @@ app.post('/hello4', (req, res) => {
     res.send(`Hello ${name}`);
 })
 
+//response-JSON 데이터 받기
+app.post('/hello5', (req,res) => {
+    const result = {
+        code: 0,
+        message: 'success'
+    };
+    res.send(result);
+})
+
+//request - json 데이터 보내기
+app.post('/hello6', (req, res) => {
+    console.log(req.body);
+    const result = req.body;
+    res.send(result);
+})
+
+
 app.listen(8080, ()=>{
     console.log('server is listening 8080');
 })
+
